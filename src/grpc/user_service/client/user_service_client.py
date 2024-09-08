d0883c8151f928718cb24933ada801a0c0cad4f7
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-08 14:25:42 +0300

    Implement gRPC logic

---
+++ src/grpc/user_service/client/user_service_client.py
@@ -0,0 +1,44 @@
+import grpc
+from src.grpc.user_service.protobuf import user_service_pb2, user_service_pb2_grpc
+from src.exceptions.exception import OrderServiceException
+from src.settings.settings import USER_SERVICE_GRPC_ADDRESS
+from src import schemas
+
+
+def get_or_create_customer_grpc(
+    token: str, request: schemas.CustomerInPOSTOrderRequestBody
+) -> schemas.CustomerFullInfo:
+    """
+    This function is used to send a request to the User Service gRPC server
+    to get or create a customer
+    *Args:
+        token (str): the token of the user
+        request (schemas.CustomerInPOSTOrderRequestBody): contains customer details
+    *Returns:
+        the created/found Customer instance
+    """
+    try:
+        with grpc.insecure_channel(USER_SERVICE_GRPC_ADDRESS) as channel:
+            stub = user_service_pb2_grpc.UserServiceStub(channel)
+            response = stub.GetOrCreateCustomer(
+                user_service_pb2.CustomerRequest(
+                    token_data=user_service_pb2.TokenData(token=token),
+                    customer=user_service_pb2.CustomerPOSTRequestBody(
+                        phone_no=request.phone_no, name=request.name
+                    ),
+                )
+            )
+            return schemas.CustomerFullInfo(
+                id=response.customer.id,
+                name=response.customer.name,
+                phone_no=response.customer.phone_no,
+                coffee_shop_id=response.customer.coffee_shop_id,
+                created="2024-09-08 09:59:48.291854",  # Fake date, TODO: Implement this
+            )
+    except grpc.RpcError as e:
+        status_code = int(e.details()[0:3])
+        message = e.details()[4:]
+        raise OrderServiceException(
+            message=message,
+            status_code=status_code,
+        )
--- src/helpers/customer.py
+++ src/helpers/customer.py
@@ -1,9 +1,5 @@
-from sqlalchemy.orm import Session
-from src import schemas, models
-from fastapi import status
-from src.utils.api_call import send_request
-from src.settings.settings import CUSTOMER_ENDPOINT
-from src.grpc.client.user_service_client import grpc_request
+from src import schemas
+from src.grpc.user_service.client.user_service_client import get_or_create_customer_grpc
 
 
 def _get_or_create_customer(
@@ -11,7 +7,7 @@ def _get_or_create_customer(
     auth_token: str,
 ) -> schemas.CustomerFullInfo:
     """
-    This helper function used to send an API request to User Management Service
+    This helper function used to send an gRPC request to User Management Service
     to get or create a customer
     *Args:
         request (schemas.CustomerInPOSTOrderRequestBody): contains customer details
@@ -20,27 +16,5 @@ def _get_or_create_customer(
         the created/found Customer instance
     """
 
-    # response = send_request(
-    #     action="POST",
-    #     url=CUSTOMER_ENDPOINT,
-    #     payload=request.dict(),
-    #     auth_token=auth_token,
-    # )
-    # customer_instance = schemas.CustomerFullInfo(**response.json())
-    # return customer_instance
-
-    # TEST CODE
-    # TODO: Remove this code after testing
-    response = grpc_request(
-        token=auth_token,
-        phone_no=request.phone_no,
-        name=request.name,
-    )
-    # dummy date test
-    return schemas.CustomerFullInfo(
-        id=response.customer.id,
-        name=response.customer.name,
-        phone_no=response.customer.phone_no,
-        coffee_shop_id=response.customer.coffee_shop_id,
-        created="2021-10-10T10:10:10",
-    )
+    response = get_or_create_customer_grpc(token=auth_token, request=request)
+    return response
