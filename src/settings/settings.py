5be016370905a967b82321052bd6386be734c2b5
Abdullah S. Naser <121259956+abdullahsn10@users.noreply.github.com>
2024-10-30 15:27:22 +0200

    Application Deployment (#19)
    * Build the Dockerfile
    
    * Update alembic migration tool settings
    
    * Update the settings
    
    * add .dockerignore file
    
    * Update db settings, copied files between stages
    
    * add script file for entrypoint commands
    
    * Update RabbitMQ configs
    
    * update redis caching configs
    
    * Add OpenAPI configs as env variable
    
    ---------
    
    Co-authored-by: Abdullah Naser <Abdullah.Naser@exalt.ps>

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -5,14 +5,26 @@ from dotenv import load_dotenv
 # load environment variables
 load_dotenv()
 
+# load database settings
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_SERVICE = os.getenv("DB_SERVICE")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+
+# database url
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_SERVICE}/{POSTGRES_DB}"
+)
+
 # database settings
 DATABASE_SETTINGS = {
-    "URL": os.getenv("SQLALCHEMY_DATABASE_URL"),
+    "URL": SQLALCHEMY_DATABASE_URL,
 }
 
+
 # security settings
-with open(os.getenv("PUBLIC_KEY_PATH"), "r") as key_file:
-    PUBLIC_KEY = key_file.read()
+PUBLIC_KEY = os.getenv("PUBLIC_KEY")
 
 
 JWT_TOKEN_SETTINGS = {
@@ -33,6 +45,8 @@ COFFEE_SHOP_ENDPOINT = USER_SERVICE_BASE_URL + "/coffee-shops"
 
 # RabbitMQ and Notification Service settings
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
 ORDER_NOTIFICATION_QUEUE = os.getenv("ORDER_NOTIFICATION_QUEUE")
 
 # Redis and Cache settings
@@ -40,12 +54,18 @@ REDIS = {
     "HOST": os.getenv("REDIS_HOST"),
     "PORT": os.getenv("REDIS_PORT"),
     "DB": os.getenv("REDIS_DB"),
+    "PASSWORD": os.getenv("REDIS_PASSWORD"),
 }
 
 ORDERS_CACHE_KEY = "orders:{coffee_shop_id}:{status}:{page}:{size}"
 ORDERS_CACHE_EXPIRATION = 300  # 5 minutes
 
 # gRPC settings
-USER_SERVICE_GRPC_HOST = os.getenv("USER_SERVICE_GRPC_HOST")
-USER_SERVICE_GRPC_PORT = os.getenv("USER_SERVICE_GRPC_PORT")
+USER_SERVICE_GRPC_HOST = os.getenv("GRPC_HOST")
+USER_SERVICE_GRPC_PORT = os.getenv("GRPC_PORT")
 USER_SERVICE_GRPC_ADDRESS = f"{USER_SERVICE_GRPC_HOST}:{USER_SERVICE_GRPC_PORT}"
+
+
+# docs settings
+OPENAPI_URL = os.getenv("OPENAPI_URL")
+ROOT_PATH = os.getenv("ROOT_PATH")
